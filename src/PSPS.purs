96a0e55b86cf2f3252db9532e74d91633c8014bb
Mike Solomon <mike@meeshkan.com>
2020-09-09 13:16:40 +0300

    Adds comment

--- src/PSPS.purs
+++ src/PSPS.purs
@@ -10,6 +10,15 @@ foreign import kind ModuleName
 
 foreign import data Constructor :: Symbol -> Symbol -> Nat -> Expr
 
+-- evaluates an expression to a result
+-- sometimes, in the case of data, the result will mirror the expression
+-- for example, imagine you have
+-- data Foo = Foo Int
+-- and then an application (App (Var Foo) 1)
+-- the result will contain the same information as the application (not sure how to encode it)
+-- because there is no further reducing to do here.
+-- on the other hand, 1+1 (addition of two typelevel integers) should result in 2
+-- we'll have to figure out what data have `kind Result`
 class Expr (expr :: Expr) (result :: Result) | expr -> result
 
 -- binds and modulename (ie Foo.Bar) and a name (ie Baz) to an expression
